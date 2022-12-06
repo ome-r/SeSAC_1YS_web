70df957ffc299f68cdbecb384a6d83bb058646c0
ome-r <dlsgo0405@naver.com>
2022-12-06 19:20:28 +0900

    feat : visitor to user

--- nodejs/221203.mysql_register/controller/Cuser.js
+++ nodejs/221203.mysql_register/controller/Cuser.js
@@ -1,4 +1,4 @@
-const Visitor = require("../model/myuser"); //modle의 visitor라는 js 연결, Visitor라는 객체를 만들어 사용한다.
+const User = require("../model/myuser"); //modle의 visitor라는 js 연결, Visitor라는 객체를 만들어 사용한다.
 
 //함수 vistior 와 register를 만듦
 
@@ -16,7 +16,7 @@ exports.user = (req,res) => { //가장 기본 라우터에 대한 함수 정의,
 }
 //회원가입이 되면 true보내기 
 exports.register = (req, res) => { // req.body는 사용자가 보내주는 값을 통째로 넘기기 위해 사용한다 
-    Visitor.register_vistior(req.body, function(){ //이 함수를 model에 넘겨준다 cb (콜백함수)를 인자로 넘겨준것
+    User.register_vistior(req.body, function(){ //이 함수를 model에 넘겨준다 cb (콜백함수)를 인자로 넘겨준것
             console.log();    
             res.send(true); // 할일이 다끝나면 응답을 한다, 이때 result는 쿼리를 날려서 나온 결과값이다. 이때 id 하나만 넘겨준다(model에서 그렇게 정의했으므로)
         }); //숫자를 보내기 때문에 string을 넣는다 send에는 숫자만 들어갈 수 없기 대문 
@@ -34,14 +34,14 @@ exports.login = (req, res) => {
 //삭제된다면 보낼 것 
 exports.profileDelete = (req,res)=>{
  //1. mysql에서 req.body.id에 해당하는 데이터를 delete를 할 것이다.(모델에서 할 것) 2. delete한 것을 서버에 응답 즉 res.send해야한다. 
-    Visitor.delete_visitor(req.body.id, function(){
+    User.delete_visitor(req.body.id, function(){
         res.send(true); //삭제만 잘 되면 응답만 보내면 되므로 true만 보낸다. 클라이언트가 끝난걸 알게 만들기 위해, res.send는 반드시 있어야한다. 안에 res.send("성공");이렇게 해도 된다
     });
 }
 
 exports.profileEdit = (req,res)=>{
     //1. mysql에서 req.body.id에 해당하는 데이터를 delete를 할 것이다.(모델에서 할 것) 2. delete한 것을 서버에 응답 즉 res.send해야한다. 
-       Visitor.update_visitor(req.body.id, function(){
+     User.update_visitor(req.body.id, function(){
            res.send(true); //삭제만 잘 되면 응답만 보내면 되므로 true만 보낸다. 클라이언트가 끝난걸 알게 만들기 위해, res.send는 반드시 있어야한다. 안에 res.send("성공");이렇게 해도 된다
        });
    }
@@ -50,7 +50,7 @@ exports.profileEdit = (req,res)=>{
 //사용자 정보 가져오기 위한 아이 
 exports.profile = (req,res) => {
     console.log(req.body.id);
-    Visitor.login(req.body.id, req.body.pw, function(result){ //get_visitor_by_id_model 이라는 함수 실행 
+    User.login(req.body.id, req.body.pw, function(result){ //get_visitor_by_id_model 이라는 함수 실행 
         if ( result.length > 0 ) res.send(true); //model에서 id, pw가 존재하는지 여부 판단만!
         else res.send(false);
     });
@@ -58,7 +58,7 @@ exports.profile = (req,res) => {
 
 exports.profileView = (req,res) => {
     console.log(req.body.id);
-    Visitor.get_user(req.body.id, function(result){ //get_visitor_by_id_model 이라는 함수 실행 
+    User.get_user(req.body.id, function(result){ //get_visitor_by_id_model 이라는 함수 실행 
         res.render("profile", {data: result})
     });
 }
